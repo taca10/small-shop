3dcca0ae27def9ec91223f7d64c901a2bb27d272
takato <taka.ur.0505@gmail.com>
2017-07-02 21:35:29 +0900

    Revert "twitter_gem"
    This reverts commit 55adbcc7ea015af24dffdf655183a0ce3fb34ab4.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
- devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
   root 'tops#index'
 
   resources :tops, only: [:show]
 
-  # get '/auth/:provider/callback', :to => 'sessions#callback'
-  # post '/auth/:provider/callback', :to => 'sessions#callback'
-  # get '/logout' => 'sessions#destroy', as: :logout
+  get '/auth/:provider/callback', :to => 'sessions#callback'
+  post '/auth/:provider/callback', :to => 'sessions#callback'
+  get '/logout' => 'sessions#destroy', as: :logout
 end
