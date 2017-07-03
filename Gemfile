55a31eb45c570fd3ff0fb0070662a5bdc6f696ad
takato <taka.ur.0505@gmail.com>
2017-07-03 19:26:15 +0900

    devise_gem

--- Gemfile
+++ Gemfile
@@ -53,6 +53,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'haml-rails'
 gem "font-awesome-rails"
-gem 'omniauth'
-gem 'omniauth-twitter'
+gem 'devise'
+gem 'pry-rails'
 gem 'dotenv-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
-  def self.create_with_omniauth(auth)
-    create! do |user|
-      user.provider = auth['provider']
-      user.uid = auth['uid']
-      user.nickname = auth['info']['nickname']
-      user.image = auth['info']['image']
-    end
-  end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable, :timeoutable
 end
